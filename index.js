ea8de93cb6ef676b5ac4b7f62de8b05ad112b96d
Richard Habermann <richie_habbie@web.de>
2015-06-18 12:39:18 +0200

    project setup
    initial files

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by richardhabermann on 18.06.15.
+ */
+module.exports = function() {
+    console.log('Läuft');
+};
---
+++ tests/test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by richardhabermann on 18.06.15.
+ */
+var mocha = require('mocha'),
+    should = require('should'),
+    supertest = require('supertest');
+
+describe('Run Test', function () {
+    it('should run', function (done) {
+
+        done();
+    });
+});
